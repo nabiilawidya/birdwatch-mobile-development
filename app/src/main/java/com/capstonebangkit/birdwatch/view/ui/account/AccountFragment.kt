ee67dfb872d5643bf58b4d2794617aed33a2c934
nabiilawidya <nabiilawidya@7702@gmail.com>
2024-06-06 16:48:40 +0700

    connect to firebase

--- app/src/main/java/com/capstonebangkit/birdwatch/view/ui/account/AccountFragment.kt
+++ app/src/main/java/com/capstonebangkit/birdwatch/view/ui/account/AccountFragment.kt
@@ -1,21 +1,29 @@
 package com.capstonebangkit.birdwatch.view.ui.account
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.credentials.ClearCredentialStateRequest
+import androidx.credentials.CredentialManager
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.capstonebangkit.birdwatch.databinding.FragmentNotificationsBinding
+import androidx.lifecycle.lifecycleScope
+import com.capstonebangkit.birdwatch.databinding.FragmentAccountBinding
+import com.capstonebangkit.birdwatch.view.login.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.launch
 
 class AccountFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentAccountBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,14 +33,37 @@ class AccountFragment : Fragment() {
         val accountViewModel =
             ViewModelProvider(this).get(AccountViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentAccountBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
         accountViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        binding.btnlogout.setOnClickListener {
+            signOut()
+        }
+
+        auth = FirebaseAuth.getInstance()
+        val firebaseUser = auth.currentUser
+        if (firebaseUser == null) {
+            // Not signed in, launch the Login activity
+            startActivity(Intent(requireContext(), LoginActivity::class.java))
+            activity?.finish()
+            return root
+        }
         return root
+    }
+
+    private fun signOut(){
+        lifecycleScope.launch {
+            val credentialManager = CredentialManager.create(requireContext())
+            auth.signOut()
+            credentialManager.clearCredentialState(ClearCredentialStateRequest())
+            startActivity(Intent(requireContext(), LoginActivity::class.java))
+            activity?.finish()
+        }
     }
 
     override fun onDestroyView() {
